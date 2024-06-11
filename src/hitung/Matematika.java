4548a4a4bc367b4bfd9d86223e846e99934c09c7
seidi255 <seidirahmat7@gmail.com>
2024-06-11 20:25:23 +0800

    membuat conturktor  dan setpenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hitung;
 
-/**
- *
- * @author ACER
- */
+
 public class Matematika {
+      //membuat variabel
+   private double bil1, bil2;
+   
+   //membuat conturktor
+   public Matematika(double bil1, double bil2){
+       this.bil1 =bil1;
+       this.bil2 =bil2;
+   }
+   
+   //membuat method set penjumlahan
+   double setpenjumlahan(){
+       return bil1 + bil2;
+   }
     
 }
